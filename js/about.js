adb3bd9432aa4fe47c93eb04565c99482011749d
Dwi Kurnia Putra <82687611+pebongg@users.noreply.github.com>
2021-08-26 12:19:15 +0700

    Add files via upload

--- js/about.js
+++ js/about.js
@@ -1,20 +1,18 @@
 const HTTP = new XMLHttpRequest();
 
-function formatNumber(num) {
-    return num
-        .toString()
-        .replace('.', ',')
-        .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
-}
-
 HTTP.open('GET', 'https://api.github.com/repos/pebongg/pebongg.github.io/languages');
 HTTP.send();
 HTTP.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         var data = JSON.parse(this.responseText);
+        function formatNumber(num) {
+            return num
+                .toFixed(1)
+        }
+        let convert = 0.000902;
         //console.log(this.responseText)
-        document.getElementById("apiHTML").innerHTML = "HTML : " + formatNumber(data.HTML) + " %";
-        document.getElementById("apiCSS").innerHTML = "CSS : " + formatNumber(data.CSS) + " %";
-        document.getElementById("apiJavaScript").innerHTML = "Javascript : " + formatNumber(data.JavaScript) + " %";
+        document.getElementById("apiHTML").innerHTML = "HTML : " + formatNumber(data.HTML*convert) + " %";
+        document.getElementById("apiCSS").innerHTML = "CSS : " + formatNumber(data.CSS*convert) + " %";
+        document.getElementById("apiJavaScript").innerHTML = "Javascript : " + formatNumber(data.JavaScript*convert) + " %";
     }
 }
